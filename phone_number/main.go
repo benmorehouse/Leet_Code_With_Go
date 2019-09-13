95ab94bdfd41f01a92b3db37e019824af1d2a2ed
Benjamin Morehouse <bmorehou@usc.edu>
2019-09-12 18:16:11 -0700

    movement

---
+++ phone_number/main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+	"bufio"
+	//"io/ioutil"
+	"os"
+	//"strings"
+)
+
+var m map[int]string
+
+func main(){
+
+}
+
+func letterCombinations(digits string) []string {
+
+}
+
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+*/
+
